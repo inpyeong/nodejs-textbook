e3e139aae83ec1f5128efb92244a6324b60af527
inpyeong <jinipyung@gmail.com>
2020-08-09 14:01:36 +0900

    Implement router

--- ch07/learn-sequelize/routes/users.js
+++ ch07/learn-sequelize/routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const result = await User.create({
+      name: req.body.name,
+      age: req.body.age,
+      married: req.body.age,
+    });
+    console.log(result);
+    res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
