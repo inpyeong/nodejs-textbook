19375826b061a682c318a893cd73fb25355b67cc
inpyeong <jinipyung@gmail.com>
2020-08-11 23:38:37 +0900

    Revert app.js to original version

--- ch08/learn-mongoose/app.js
+++ ch08/learn-mongoose/app.js
@@ -1,44 +1,51 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const nunjucks = require('nunjucks');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var nunjucks = require('nunjucks');
 
-const connect = require('./schemas');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const commentsRouter = require('./routes/comments');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var commentsRouter = require('./routes/comments');
+var connect = require('./schemas');
 
-const app = express();
-app.set('port', process.env.PORT || 3002);
+var app = express();
+connect();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
   watch: true,
 });
-connect();
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public/javascripts')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/comments', commentsRouter);
 
-app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.use((err, req, res, next) => {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
-});
+module.exports = app;
