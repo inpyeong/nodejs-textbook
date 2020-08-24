8c544e661b24917e6420c68e3c9ad6c9531bb635
inpyeong <jinipyung@gmail.com>
2020-08-25 06:51:50 +0900

    Correct createAt to createdAt

--- ch09/nodebird/routes/page.js
+++ ch09/nodebird/routes/page.js
@@ -22,7 +22,7 @@ router.get('/', (req, res, next) => {
       model: User,
       attributes: ['id', 'nick'],
     },
-    order: [['createAt', 'DESC']],
+    order: [['createdAt', 'DESC']],
   })
     .then((posts) => {
       res.render('main', {
